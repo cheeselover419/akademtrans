5ac40faabffdea82a554c092d365d0f1244e3661
Алексей Крюков <aleksejkrukov@MacBook-Pro-Aleksej.local>
2024-07-07 20:44:26 +0300

    first commit

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/aleksejkrukov/Downloads/django-quiz-app-master/venv/bin/python3.11
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
